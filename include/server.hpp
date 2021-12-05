a7e391e70fc0080405d236fd5b54b2f2583c2de1
VishakhaDixit <vishakhadixit1996@gmail.com>
2021-12-04 23:55:40 -0500

    Adding Server Class

---
+++ include/server.hpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <fstream>
+#include <unistd.h> 
+#include <opencv4/opencv2/opencv.hpp>
+
+#define PORT 8080
+#define IP   "127.0.0.1"
+#define IMG_PATH "/home/vdixit2/Downloads"
+
+using namespace std;
+
+class server
+{
+    public:
+        server()  = default;
+        ~server() = default;
+        void run();
+        cv::Mat recieve_image();
+        void process_image();
+        void send_image();
+
+    private:
+        struct sockaddr_in address;
+        int server_fd;
+
+}
