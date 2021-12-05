71e46ac38832daaa97fc9cc1658ff383d29f473e
VishakhaDixit <vishakhadixit1996@gmail.com>
2021-12-04 22:01:54 -0500

    fixing IP define and adding unistd.h

--- include/client.hpp
+++ include/client.hpp
@@ -2,15 +2,18 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <fstream>
+#include <unistd.h> 
 #include <opencv4/opencv2/opencv.hpp>
 
 #define PORT 8080
-#define IP   127.0.0.1
+#define IP   "127.0.0.1"
 #define IMG_PATH "/home/vdixit2/Downloads"
+
+using namespace std;
 
 namespace client
 {
-    void img_transmit(std::string path);
+    void img_transmit(string path);
 
     void send_image(int socket, string img);
 
