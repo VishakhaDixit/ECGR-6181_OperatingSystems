b45c64c85432d343397433157aa5806b7eb56282
VishakhaDixit <vishakhadixit1996@gmail.com>
2021-12-06 00:57:44 -0500

    Adding commonly used code to common directory.

---
+++ src/common/image_convertor.cpp
@@ -0,0 +1,31 @@
+#include "image_converter.hpp"
+
+#include <thread>
+#include <iostream>
+#include <chrono>
+
+#include <opencv2/opencv.hpp>
+
+namespace convertors
+{
+    cv::Mat convert_to_grayscale(cv::Mat in) 
+    {
+        // create an empty image of the same size and
+        // type
+        cv::Mat ret{ 
+            in.rows,
+            in.cols,
+            in.type()
+        };
+
+        // convert the color to grayscale
+        cv::cvtColor(in, ret, cv::COLOR_RGB2GRAY);
+
+        // sleep for 10 seconds
+        std::cout << "CVT2GRAY: Image converted to grayscale, sleeping" << std::endl;
+        //std::this_thread::sleep_for(std::chrono::seconds{ 10 });
+        std::cout << "CVT2GRAY: Simulated load compleate, waking up" << std::endl;
+
+        return ret;
+    }
+}
